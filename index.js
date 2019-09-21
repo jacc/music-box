bb0808583362aed26b2baa49a36d075922c932e3
Casper da Costa-Luis <casper.dcl@physics.org>
2019-09-21 22:36:25 +0100

    increase number of artists to 10 (more info when gist is clicked on)

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function main() {
     console.error(`music-box ran into an issue getting your Gist:\n${error}`);
   }
 
-  const numArtitst = Math.min(5, json.topartists.artist.length);
+  const numArtitst = Math.min(10, json.topartists.artist.length);
   let playsTotal = 0;
   for(let i = 0; i < numArtitst; i++) {
     playsTotal += parseInt(json.topartists.artist[i].playcount, 10);
